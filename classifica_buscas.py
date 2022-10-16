3850b933194c70d0ab5ba6c9d10b035a9cb9fadb
moises-creator <moises.ximenes5@gmail.com>
2022-10-16 18:30:51 -0400

    subindo alteracao em treino base e algoritmo

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,19 +1,16 @@
+from collections import Counter
 import pandas as pd
-df = pd.read_csv('busca.csv')
 
+df = pd.read_csv('busca.csv')
 
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
 
-
 Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
 
-
 X = Xdummies_df.values
 Y = Ydummies_df.values
-
-
 
 porcentagem_treino = 0.9
 
@@ -27,18 +24,21 @@ teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
 from sklearn.naive_bayes import MultinomialNB
+
 modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
+acertos = (resultado == teste_marcacoes)
 
-diferencas = resultado - teste_marcacoes
-
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
-
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print(taxa_de_acerto)
+print("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
 print(total_de_elementos)
+
+# a eficácia do algoritmo que chuta tudo um único valor
+acerto_base = max(Counter(teste_marcacoes).values())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
